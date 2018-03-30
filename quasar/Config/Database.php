e4513cb976894a273fb4b3a02b39a3b80bf1c943
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-30 12:30:30 +0300

    Initial implementation of the Database API

---
+++ quasar/Config/Database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'default' => array(
+        'driver'    => 'mysql',
+        'hostname'  => 'localhost',
+        'database'  => 'quasar',
+        'username'  => 'quasar',
+        'password'  => 'password',
+        'prefix'    => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_general_ci',
+
+        // The string used to wrap tables and keywords.
+        'wrapper'   => '`',
+    ),
+);
