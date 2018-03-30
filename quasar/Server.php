c05f8da3f4f753fa5b440e67ec4f7aa69fea3055
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-30 13:46:27 +0300

    Overall improvements

---
+++ quasar/Server.php
@@ -0,0 +1,63 @@
+<?php
+
+use Quasar\System\Exceptions\NotFoundHttpException;
+use Quasar\System\Http\Request;
+use Quasar\System\Http\Response;
+use Quasar\System\Http\Router;
+use Quasar\System\Config;
+use Quasar\System\Container;
+
+use Workerman\Worker;
+use PHPSocketIO\SocketIO;
+
+
+//--------------------------------------------------------------------------
+// The Push Server
+//--------------------------------------------------------------------------
+
+// The PHPSocketIO service.
+Container::instance(
+    SocketIO::class, $socketIo = new SocketIO(SENDER_PORT)
+);
+
+// Get the configured clients.
+$clients = Config::get('clients');
+
+// When the client initiates a connection event, set various event callbacks for connecting sockets.
+foreach ($clients as $appId => $secretKey) {
+    $senderIo = $socketIo->of($appId);
+
+    $senderIo->presence = array();
+
+    // Include the Events file.
+    require_once QUASAR_PATH .'Events.php';
+}
+
+// When $socketIo is started, it listens on an HTTP port, through which data can be pushed to any channel.
+$socketIo->on('workerStart', function ()
+{
+    // Listen on a HTTP port.
+    $innerHttpWorker = new Worker('http://' .SERVER_HOST .':' .SERVER_PORT);
+
+    // Create a Router instance.
+    $router = new Router(QUASAR_PATH .'Routes.php');
+
+    // Triggered when HTTP client sends data.
+    $innerHttpWorker->onMessage = function ($connection) use ($router)
+    {
+        try {
+            $request = Request::createFromGlobals();
+
+            $response = $router->dispatch($request);
+        }
+        catch (NotFoundHttpException $e) {
+            $response = new Response('404 Not Found', 404);
+        }
+
+        return $response->send($connection);
+    };
+
+    // Perform the monitoring.
+    $innerHttpWorker->listen();
+});
+
--- server.php
+++ server.php
@@ -1,18 +1,10 @@
 #!/usr/bin/env php
 <?php
 
-use Quasar\System\Exceptions\NotFoundHttpException;
-use Quasar\System\Http\Request;
-use Quasar\System\Http\Response;
-use Quasar\System\Http\Router;
 use Quasar\System\Config;
-use Quasar\System\Container;
 
-use Workerman\Protocols\Http;
 use Workerman\Worker;
-use Workerman\WebServer;
-use Workerman\Lib\Timer;
-use PHPSocketIO\SocketIO;
+
 
 //--------------------------------------------------------------------------
 // Global Configuration
@@ -53,51 +45,7 @@ foreach (glob(QUASAR_PATH .'Config/*.php') as $path) {
 // Create the Push Server
 //--------------------------------------------------------------------------
 
-// The PHPSocketIO service.
-Container::instance(
-    SocketIO::class, $socketIo = new SocketIO(SENDER_PORT)
-);
-
-// Get the configured clients.
-$clients = Config::get('clients');
-
-// When the client initiates a connection event, set various event callbacks for connecting sockets.
-foreach ($clients as $appId => $secretKey) {
-    $senderIo = $socketIo->of($appId);
-
-    $senderIo->presence = array();
-
-    // Include the Events file.
-    require_once QUASAR_PATH .'Events.php';
-}
-
-// When $socketIo is started, it listens on an HTTP port, through which data can be pushed to any channel.
-$socketIo->on('workerStart', function ()
-{
-    // Listen on a HTTP port.
-    $innerHttpWorker = new Worker('http://' .SERVER_HOST .':' .SERVER_PORT);
-
-    // Create a Router instance.
-    $router = new Router(QUASAR_PATH .'Routes.php');
-
-    // Triggered when HTTP client sends data.
-    $innerHttpWorker->onMessage = function ($connection) use ($router)
-    {
-        try {
-            $request = Request::createFromGlobals();
-
-            $response = $router->dispatch($request);
-        }
-        catch (NotFoundHttpException $e) {
-            $response = new Response('404 Not Found', 404);
-        }
-
-        return $response->send($connection);
-    };
-
-    // Perform the monitoring.
-    $innerHttpWorker->listen();
-});
+require QUASAR_PATH .'Server.php';
 
 
 //--------------------------------------------------------------------------
