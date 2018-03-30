fba6e60f8a5cc52fc335e6033cd628192422b53c
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-31 01:57:59 +0300

    Overall improvements

---
+++ quasar/Http/Controllers/Sample.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Quasar\Http\Controllers;
+
+use Quasar\Platform\Http\Request;
+use Quasar\Platform\Http\Response;
+
+use Quasar\Http\Controllers\BaseController;
+
+
+class Sample extends BaseController
+{
+
+    public function index(Request $request, $slug = null)
+    {
+        $content = htmlspecialchars($slug);
+
+        return $this->createView()
+            ->shares('title', 'Homepage')
+            ->with('content', $content);
+    }
+}
