1194461f501f7dfacbbdc6e6d3c41be90b15e4e4
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-05-11 17:44:02 +0300

    Improve the server/Server.php

--- server/Server.php
+++ server/Server.php
@@ -9,6 +9,8 @@ use System\Http\Request;
 use PHPSocketIO\SocketIO;
 use Workerman\Worker;
 
+// Get the clients list.
+$clients = $config->get('clients', array());
 
 // Create and setup the PHPSocketIO service.
 $app->instance(SocketIO::class, $socketIo = new SocketIO(SOCKET_PORT, array(
@@ -16,14 +18,53 @@ $app->instance(SocketIO::class, $socketIo = new SocketIO(SOCKET_PORT, array(
     'socket' => 'Server\Platform\SocketIO\Socket',
 )));
 
-// Get the clients list.
-$clients = $config->get('clients', array());
 
-// When the client initiates a connection event, set various event callbacks for connecting sockets.
+// When $socketIo is started, it listens on an HTTP port, through which data can be pushed to any channel.
+$socketIo->on('workerStart', function () use ($app)
+{
+    $router = $app['router'];
+
+    // Bootstrap the Router instance.
+    require SERVER_PATH .'Http' .DS .'Bootstrap.php';
+
+    // Listen on a HTTP port.
+    $innerHttpWorker = new Worker('http://' .SERVER_HOST .':' .SERVER_PORT);
+
+    // Triggered when HTTP client sends data.
+    $innerHttpWorker->onMessage = function ($connection) use ($router)
+    {
+        ob_start();
+
+        // Dispatch the HTTP request via Routing.
+        $request = Request::createFromGlobals();
+
+        $response = $router->handle($request);
+
+        // Render the Response content.
+        $response->send();
+
+        // Send the response headers and content to TCP Connection instance.
+        $response->close($connection, ob_get_clean());
+    };
+
+    // Perform the monitoring.
+    $innerHttpWorker->listen();
+});
+
+// When $socketIo is stopped, execute the associated callback.
+$socketIo->on('workerStop', function ()
+{
+    //
+});
+
+//
+// When the SocketIO client initiates a connection event, set various event callbacks for connecting sockets.
+
 array_walk($clients, function ($client) use ($socketIo)
 {
-    $publicKey = $client['key']; // We will use the client's public key as namespace.
+    $publicKey = $client['key']; // We will use the client's public key also as namespace.
 
+    //
     $senderIo = $socketIo->of($publicKey);
 
     $senderIo->setup(
@@ -218,42 +259,4 @@ array_walk($clients, function ($client) use ($socketIo)
             }
         });
     });
-});
-
-// When $socketIo is started, it listens on an HTTP port, through which data can be pushed to any channel.
-$socketIo->on('workerStart', function () use ($app)
-{
-    $router = $app['router'];
-
-    // Bootstrap the Router instance.
-    require SERVER_PATH .'Http' .DS .'Bootstrap.php';
-
-    // Listen on a HTTP port.
-    $innerHttpWorker = new Worker('http://' .SERVER_HOST .':' .SERVER_PORT);
-
-    // Triggered when HTTP client sends data.
-    $innerHttpWorker->onMessage = function ($connection) use ($router)
-    {
-        ob_start();
-
-        // Dispatch the HTTP request via Routing.
-        $request = Request::createFromGlobals();
-
-        $response = $router->handle($request);
-
-        // Render the Response content.
-        $response->send();
-
-        // Send the response headers and content to TCP Connection instance.
-        $response->close($connection, ob_get_clean());
-    };
-
-    // Perform the monitoring.
-    $innerHttpWorker->listen();
-});
-
-// When $socketIo is stopped, execute the associated callback.
-$socketIo->on('workerStop', function ()
-{
-    //
 });
