e3a6934a30299d87a81c880ff3f1f9c503dbb49d
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-05-26 15:59:46 +0300

    Improve the Database

---
+++ system/Database/Builder.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Quasar\Database;
+
+use Quasar\Database\Connection;
+use Quasar\Database\Query\Builder as QueryBuilder;
+use Quasar\Database\Model;
+
+
+class Builder extends QueryBuilder
+{
+    /**
+     * The model instance being queried.
+     *
+     * @var Quasar\Database\Model
+     */
+    public $model;
+
+
+    /**
+     * Create a new Builder instance.
+     *
+     * @return void
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+
+        //
+        parent::__construct($model->getConnection(), $model->getTable());
+    }
+
+    /**
+     * Execute a query for a single record by ID.
+     *
+     * @param  int    $id
+     * @param  array  $columns
+     * @return mixed
+     */
+    public function find($id, $columns = array('*'))
+    {
+        $keyName = $this->model->getKeyName();
+
+        return $this->where($keyName, '=', $id)->first($columns);
+    }
+
+    /**
+     * Find a Model by its primary key.
+     *
+     * @param  array  $id
+     * @param  array  $columns
+     * @return array|static
+     */
+    public function findMany($ids, $columns = array('*'))
+    {
+        if (empty($ids)) return array();
+
+        $this->query->whereIn($this->model->getKeyName(), $ids);
+
+        return $this->get($columns);
+    }
+
+    /**
+     * Pluck a single column's value from the first result of a query.
+     *
+     * @param  string  $column
+     * @return mixed
+     */
+    public function pluck($column)
+    {
+        $result = $this->first(array($column));
+
+        if (! is_null($result)) {
+            // Convert the Model instance to array.
+            $result = $result->toArray();
+        }
+
+        return (count($result) > 0) ? reset($result) : null;
+    }
+
+    /**
+     * Execute the query as a "SELECT" statement.
+     *
+     * @param  array  $columns
+     * @return \Database\ORM\Model|static[]
+     */
+    public function get($columns = array('*'))
+    {
+        return $this->getModels($columns);
+    }
+
+    /**
+     + Get the hydrated Models without eager loading.
+     *
+     * @param  array  $columns
+     * @return \Database\ORM\Model|static[]
+     */
+    public function getModels($columns = array('*'))
+    {
+        $results = parent::get($columns);
+
+        $connection = $this->model->getConnectionName();
+
+        return array_map(function ($result) use ($connection)
+        {
+            $model = $this->model->newFromBuilder((array) $result);
+
+            return $model->setConnection($connection);
+
+        }, $results);
+    }
+
+    /**
+     * Execute an aggregate function on the database.
+     *
+     * @param  string  $function
+     * @param  array   $columns
+     * @return mixed
+     */
+    public function aggregate($function, $columns = array('*'))
+    {
+        $this->aggregate = compact('function', 'columns');
+
+        $previousColumns = $this->columns;
+
+        //
+        $results = $this->get($columns);
+
+        $this->aggregate = null;
+
+        $this->columns = $previousColumns;
+
+        if (! empty($results)) {
+            $model = first($results);
+
+            // Convert the Model instance to array.
+            $result = (array) $model->toArray();
+
+            return $result['aggregate'];
+        }
+    }
+
+    /**
+     * Delete a Record from the database.
+     *
+     * @return int
+     */
+    public function delete()
+    {
+        return parent::delete();
+    }
+}
+
--- system/Database/Model.php
+++ system/Database/Model.php
@@ -2,160 +2,271 @@
 
 namespace Quasar\Database;
 
-use Quasar\Support\Facades\DB;
+use Quasar\Database\Query\Builder as QueryBuilder;
 
 
-class Model
+class Model implements \ArrayAccess
 {
     /**
-     * The Connection name.
+     * The Database Connection name.
      *
      * @var string
      */
     protected $connection = 'default';
 
     /**
-     * The table associated with the Model.
+     * The table associated with the model.
      *
      * @var string
      */
     protected $table;
 
     /**
-     * The primary key for the Model.
+     * The primary key for the model.
      *
      * @var string
      */
     protected $primaryKey = 'id';
 
+    /**
+     * The model's attributes.
+     *
+     * @var array
+     */
+    protected $attributes = array();
+
+    /**
+     * The model attribute's original state.
+     *
+     * @var array
+     */
+    protected $original = array();
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = array();
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = array('*');
+
+    /**
+     * Indicates if all mass assignment is enabled.
+     *
+     * @var bool
+     */
+    protected static $unguarded = true;
+
+    /**
+     * Indicates if the model exists.
+     *
+     * @var bool
+     */
+    public $exists = false;
+
 
     /**
      * Create a new Model instance.
      *
-     * @param  string  $connection
+     * @param  array  $attributes
      * @return void
      */
-    public function __construct($connection = null)
+    public function __construct(array $attributes = array())
     {
-        if (! is_null($connection)) {
-            $this->connection = $connection;
-        }
+        $this->syncOriginal();
 
-        if (isset($this->table)) {
-            return;
-        }
-
-        // Guessing the table like: 'App\Models\PostComments' -> 'post_comments'
-
-        $this->table = strtolower(preg_replace('~(?<=\\w)([A-Z])~', '_$1',
-            basename(str_replace('\\', '/', static::class))
-        ));
+        $this->fill($attributes);
     }
 
     /**
-     * Find a Record by its primary key.
+     * Begin querying the model.
      *
-     * @param  mixed  $id
-     * @param  array  $columns
-     * @return Model
+     * @return \Quasar\Database\Query\Builder
      */
-    public function find($id, $columns = array('*'))
+    public static function query()
     {
-        return $this->newQuery()->where($this->getKeyName(), $id)->first($columns);
+        $instance = new static;
+
+        return $instance->newQuery();
     }
 
     /**
-     * Find many Records by their primary key.
+     * Begin querying the model on a given connection.
      *
-     * @param  array  $ids
-     * @param  array  $columns
-     * @return Model
+     * @param  string  $connection
+     * @return $this
      */
-    public function findMany(array $ids, $columns = array('*'))
+    public static function on($connection = 'default')
     {
-        return $this->newQuery()->where($this->getKeyName(), $ids)->get($columns);
+        $instance = new static;
+
+        return $instance->setConnection($connection)->newQuery();
     }
 
     /**
-     * Get all of the Records from the database.
+     * Get all of the models from the database.
      *
      * @param  array  $columns
      * @return array
      */
-    public function findAll($columns = array('*'))
+    public static function all($columns = array('*'))
     {
-        return $this->newQuery()->get($columns);
+        $instance = new static;
+
+        return $instance->newQuery()->get($columns);
     }
 
     /**
-     * Insert a new Record and get the value of the primary key.
-     *
-     * @param  array   $values
-     * @return int
-     */
-    public function insert(array $values)
-    {
-        return $this->newQuery()->insert($values);
-    }
-
-    /**
-     * Update the Record in the database.
+     * Find a model by its primary key.
      *
      * @param  mixed  $id
-     * @param  array  $attributes
-     * @return mixed
+     * @param  array  $columns
+     * @return Model
      */
-    public function update($id, array $attributes = array())
+    public static function find($id, $columns = array('*'))
     {
-        return $this->newQuery()->where($this->getKeyName(), $id)->update($attributes);
+        $instance = new static;
+
+        return $instance->newQuery()->where($instance->getKeyName(), $id)->first($columns);
     }
 
     /**
-     * Delete the Record from the database.
+     * Set a given attribute on the model.
      *
-     * @return bool|null
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
      */
-    public function delete($id)
+    public function setAttribute($key, $value)
     {
-        $this->newQuery()->where($this->getKeyName(), $id)->delete();
+        if ($this->hasSetMutator($key)) {
+            $method = 'set' .studly_case($key) .'Attribute';
+
+            return call_user_func(array($this, $method), $value);
+        }
+
+        $this->attributes[$key] = $value;
+    }
+
+    /**
+     * Get an attribute from the model.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getAttribute($key)
+    {
+        if (array_key_exists($key, $this->attributes) || $this->hasGetMutator($key)) {
+            return $this->getAttributeValue($key);
+        }
+    }
+
+    /**
+     * Save the model to the database.
+     *
+     * @param  array  $options
+     * @return bool
+     */
+    public function save(array $options = array())
+    {
+        $query = $this->newQuery();
+
+        if ($this->exists) {
+            $this->performUpdate($query);
+        } else {
+            $this->performInsert($query);
+        }
+
+        $this->syncOriginal();
 
         return true;
     }
 
     /**
-     * Get a new Query for the Model's table.
+     * Perform a model update operation.
      *
+     * @param  \Quasar\Database\Query\Builder  $query
+     * @return bool
+     */
+    protected function performUpdate(QueryBuilder $query)
+    {
+        $dirty = $this->getDirty();
+
+        if (count($dirty) > 0) {
+            $this->setKeysForSaveQuery($query)->update($dirty);
+        }
+
+        return true;
+    }
+
+    /**
+     * Perform a model insert operation.
+     *
+     * @param  \Quasar\Database\Query\Builder  $query
+     * @return bool
+     */
+    protected function performInsert(QueryBuilder $query)
+    {
+        $keyName = $this->getKeyName();
+
+        $id = $query->insertGetId($this->attributes);
+
+        $this->setAttribute($keyName, $id);
+
+        $this->exists = true;
+
+        return true;
+    }
+
+    /**
+     * Delete the model from the database.
+     *
+     * @return bool|null
+     */
+    public function delete()
+    {
+        if ($this->exists) {
+            $query = $this->newQuery()->where($this->getKeyName(), $this->getKey());
+
+            $query->delete();
+
+            $this->exists = false;
+
+            return true;
+        }
+    }
+
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  \Quasar\Database\Query\Builder  $query
      * @return \Quasar\Database\Query\Builder
      */
-    public function newQuery()
+    protected function setKeysForSaveQuery(QueryBuilder $query)
     {
-        return $this->getConnection()->table($this->table);
+        $query->where($this->getKeyName(), $this->getKeyForSaveQuery());
+
+        return $query;
     }
 
     /**
-     * Get the Table for the Model.
+     * Get the primary key value for a save query.
      *
-     * @return string
+     * @return mixed
      */
-    public function getTable()
+    protected function getKeyForSaveQuery()
     {
-        return $this->table;
+        return array_get($this->original, $key = $this->getKeyName(), $this->getAttribute($key));
     }
 
     /**
-     * Get the Table for the Model.
-     *
-     * @return string
-     */
-    public static function getTableName()
-    {
-        $model = new static();
-
-        return $model->getTable();
-    }
-
-    /**
-     * Get the Primary Key for the Model.
+     * Get the primary key for the model.
      *
      * @return string
      */
@@ -165,13 +276,243 @@ class Model
     }
 
     /**
+     * Get the value of the model's primary key.
+     *
+     * @return mixed
+     */
+    public function getKey()
+    {
+        return $this->getAttribute($this->getKeyName());
+    }
+
+    /**
+     * Get the attributes that have been changed since last sync.
+     *
+     * @return array
+     */
+    public function getDirty()
+    {
+        $dirty = array();
+
+        foreach ($this->attributes as $key => $value) {
+            if (! array_key_exists($key, $this->original) || ($value !== $this->original[$key])) {
+                $dirty[$key] = $value;
+            }
+        }
+
+        return $dirty;
+    }
+
+    /**
+     * Get a plain attribute.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    protected function getAttributeValue($key)
+    {
+        $value = $this->getAttributeFromArray($key);
+
+        if ($this->hasGetMutator($key)) {
+            return $this->mutateAttribute($key, $value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Get an attribute from the $attributes array.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    protected function getAttributeFromArray($key)
+    {
+        if (array_key_exists($key, $this->attributes)) {
+            return $this->attributes[$key];
+        }
+    }
+
+    /**
+     * Get the value of an attribute using its mutator.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return mixed
+     */
+    protected function mutateAttribute($key, $value)
+    {
+        $method = 'get' .studly_case($key) .'Attribute';
+
+        return call_user_func(array($this, $method), $value);
+    }
+
+    /**
+     * Determine if a set mutator exists for an attribute.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function hasSetMutator($key)
+    {
+        return method_exists($this, 'set' .studly_case($key) .'Attribute');
+    }
+
+    /**
+     * Determine if a get mutator exists for an attribute.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function hasGetMutator($key)
+    {
+        return method_exists($this, 'get' .studly_case($key) .'Attribute');
+    }
+
+    /**
+     * Sync the original attributes with the current.
+     *
+     * @return Model
+     */
+    public function syncOriginal()
+    {
+        $this->original = $this->attributes;
+
+        return $this;
+    }
+
+    /**
+     * Fill the model with an array of attributes.
+     *
+     * @param  array  $attributes
+     * @return Model
+     */
+    public function fill(array $attributes)
+    {
+        foreach ($this->fillableFromArray($attributes) as $key => $value) {
+            if ($this->isFillable($key)) {
+                $this->setAttribute($key, $value);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the fillable attributes of a given array.
+     *
+     * @param  array  $attributes
+     * @return array
+     */
+    protected function fillableFromArray(array $attributes)
+    {
+        if ((count($this->fillable) > 0) && ! static::$unguarded) {
+            return array_intersect_key($attributes, array_flip($this->fillable));
+        }
+
+        return $attributes;
+    }
+
+    /**
+     * Determine if the given attribute may be mass assigned.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function isFillable($key)
+    {
+        if (static::$unguarded) return true;
+
+        if (in_array($key, $this->fillable)) return true;
+
+        if ($this->isGuarded($key)) return false;
+
+        return empty($this->fillable) && ! starts_with($key, '_');
+    }
+
+    /**
+     * Determine if the given key is guarded.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function isGuarded($key)
+    {
+        return in_array($key, $this->guarded) || ($this->guarded == array('*'));
+    }
+
+    /**
+     * Get a new query for the model's table.
+     *
+     * @return \Quasar\Database\Query\Builder
+     */
+    public function newQuery()
+    {
+        $query = new Builder($this);
+
+        return $return->table($this->table);
+    }
+
+    /**
+     * Create a new instance of the given model.
+     *
+     * @param  array  $attributes
+     * @param  bool   $exists
+     * @return Model
+     */
+    public function newInstance($attributes = array(), $exists = false)
+    {
+        $model = new static((array) $attributes);
+
+        $model->exists = $exists;
+
+        return $model;
+    }
+
+    /**
+     * Create a new model instance that is existing.
+     *
+     * @param  array  $attributes
+     * @return Model
+     */
+    public function newModel($attributes = array())
+    {
+        $instance = $this->newInstance(array(), true);
+
+        $instance->setRawAttributes((array) $attributes, true);
+
+        return $instance;
+    }
+
+    /**
+     * Set the array of model attributes. No checking is done.
+     *
+     * @param  array  $attributes
+     * @param  bool   $sync
+     * @return void
+     */
+    public function setRawAttributes(array $attributes, $sync = false)
+    {
+        $this->attributes = $attributes;
+
+        if ($sync) $this->syncOriginal();
+    }
+
+    public function getTable()
+    {
+        return $this->table;
+    }
+
+    /**
      * Get the Connection instance.
      *
      * @return \Quasar\Database\Connection
      */
     public function getConnection()
     {
-        return DB::connection($this->connection);
+        $manager = Manager::getInstance();
+
+        return $manager->connection($this->connection);
     }
 
     /**
@@ -182,6 +523,152 @@ class Model
     public function getConnectionName()
     {
         return $this->connection;
+    }
+
+    /**
+     * Set the Connection associated with the Model.
+     *
+     * @param  string  $name
+     * @return $this
+     */
+    public function setConnection($name)
+    {
+        $this->connection = $name;
+
+        return $this;
+    }
+
+    /**
+     * Convert the model instance to JSON.
+     *
+     * @param  int  $options
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this->toArray(), $options);
+    }
+
+    /**
+     * Convert the model instance to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $attributes = array();
+
+        foreach ($this->getAttributes() as $key => $value) {
+            $attributes[$key] = $this->getAttribute($key);
+        }
+
+        return $attributes;
+    }
+
+    /**
+     * Get all of the current attributes on the model.
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Determine if the given attribute exists.
+     *
+     * @param  mixed  $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->$offset);
+    }
+
+    /**
+     * Get the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->$offset;
+    }
+
+    /**
+     * Set the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->$offset = $value;
+    }
+
+    /**
+     * Unset the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->$offset);
+    }
+
+    /**
+     * Dynamically retrieve attributes on the model.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return $this->getAttribute($key);
+    }
+
+    /**
+     * Dynamically set attributes on the model.
+     *
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return void
+     */
+    public function __set($key, $value)
+    {
+        $this->setAttribute($key, $value);
+    }
+
+    /**
+     * Determine if an attribute exists on the model.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function __isset($key)
+    {
+        if (isset($this->attributes[$key])) {
+            return true;
+        } else if ($this->hasGetMutator($key)) {
+            return ! is_null($this->getAttributeValue($key));
+        }
+
+        return false;
+    }
+
+    /**
+     * Unset an attribute on the model.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function __unset($key)
+    {
+        unset($this->attributes[$key]);
     }
 
     /**
@@ -196,5 +683,19 @@ class Model
         $query = $this->newQuery();
 
         return call_user_func_array(array($query, $method), $parameters);
+    }
+
+    /**
+     * Handle dynamic static method calls into the method.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        $instance = new static;
+
+        return call_user_func_array(array($instance, $method), $parameters);
     }
 }
